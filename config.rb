e8dea4ae47b08455f470d39ba1903ae0f3290135
koi-chan <koi-chan@mail.kazagakure.net>
2016-09-29 11:53:56 +0900

    設定ファイルを分離

---
+++ config.rb
@@ -0,0 +1,28 @@
+# 調査対象サーバー
+SERVER = {
+  'irc.cre.jp'         => 'cre',
+  'irc.egotex.net'     => 'egotex',
+  'irc.kazagakure.net' => 'kazagakure',
+  'irc.r-roman.net'    => 'r-roman',
+  'irc.sougetu.net'    => 'sougetu',
+  't-net.xyz'          => 't-net'
+}
+MAP_ONLY = {
+  'services.cre.jp'    => 'services'
+}
+LUSERS_ONLY = {}
+
+# MAP コマンドを使うか否か
+USE_MAP = true
+
+# 接続パスワード
+PASS = nil
+
+# OPER 権限を得る場合、そのアカウント名とパスワード
+OPER = {
+  name: '',
+  pass: ''
+}
+
+# 接続するサーバアドレス
+TARGET = 'irc.kazagakure.net'
